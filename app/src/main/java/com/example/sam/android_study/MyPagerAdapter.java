64a36d6e5706c1e2094e0a629f569381d2a40c6a
yajun.sun <yajun.sun@cootek.cn>
2017-09-28 15:36:38 +0800

    add viewpager+List<View> to implement frame

---
+++ app/src/main/java/com/example/sam/android_study/MyPagerAdapter.java
@@ -0,0 +1,50 @@
+/**
+ * FileName:MyPagerAdapter.java
+ * Copyright(C) 2016 zteict
+ */
+package com.example.sam.android_study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author YaoDiWei
+ * @version
+ * @see com.yao.tab01.MyPagerAdapter.java
+ */
+public class MyPagerAdapter extends PagerAdapter {
+
+	private List<View> views = new ArrayList<View>();
+
+	public MyPagerAdapter(List<View> views) {
+		super();
+		this.views = views;
+	}
+
+	@Override
+	public int getCount() {
+		return views.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View arg0, Object arg1) {
+		return arg0 == arg1;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		container.removeView(views.get(position));
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		View view = views.get(position);
+		container.addView(view);
+		return view;
+	}
+
+}
